136d64e0b15b7f7de25939d96670cfd846249ab8
Dionna <businessladythick@yahoo.com>
2024-02-23 15:15:05 -0500

    did'nt do anything

---
+++ flow.js
@@ -0,0 +1,9 @@
+//info given: plants double in number every week;  plants are contained is circular, with a radius of 5 meters;  formula for calculating the area of a circle is PI multiplied by the radius
+//area of the space is
+const Pl = 3.1415;
+const radius = 5
+const startingArea = Pl * radius * radius;
+console.log(startingArea);
+
+//the mathmathical total radius of the starting area is 78.537 sq.ft.
+//now will calc the num of plants that will fit in current area, will use space req per plant
